5e196b96495470be8902021d3f72c9ab2e933bd7
Hoang Tam <kakasika15@gmail.com>
2023-09-15 16:32:12 +0700

    update code

--- SenmaticSearchCLIP/app.py
+++ SenmaticSearchCLIP/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from model.get_faiss_images import faiss_image
-from model.retrieval import get_image_list
+# from model.retrieval import get_image_list
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "./static/data"
@@ -21,16 +21,17 @@ def get_imgPath():
 def hello():
     data = request.get_json()
     query = data['query']
-    sketch = data['sketch']
-    isEnabled = data['isEnabled']
+    # sketch = data['sketch']
+    # isEnabled = data['isEnabled']
+    print(query)
     encoded_images = []
-    if isEnabled :
-        encoded_images_faiss = faiss_image(query=query)
-        encoded_images_sketch = get_image_list(sketch=sketch, caption=query)
-        encoded_images.extend(encoded_images_faiss)
-        encoded_images.extend(encoded_images_sketch)
-    else:
-        encoded_images = faiss_image(query=query)
+    # if isEnabled :
+    #     encoded_images_faiss = faiss_image(query=query)
+    #     encoded_images_sketch = get_image_list(sketch=sketch, caption=query)
+    #     encoded_images.extend(encoded_images_faiss)
+    #     encoded_images.extend(encoded_images_sketch)
+    # else:
+    encoded_images = faiss_image(query=query)
     return jsonify({'data': encoded_images})
 
 
--- SenmaticSearchCLIP/model/get_faiss_images.py
+++ SenmaticSearchCLIP/model/get_faiss_images.py
@@ -3,18 +3,25 @@ import os
 import cv2
 import torch
 import clip
+import sys
+from pathlib import Path
+
+CODE_PATH = Path('D:\HCMAI_2023_BASICAIO\SenmaticSearchCLIP')
+
+sys.path.append(str(CODE_PATH))
 from model.my_faiss import Myfaiss
 from model.translation import Translation
 from utils.query_db import get_image_path
 
 
+
 UPLOAD_FOLDER = "./static/data"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])        
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL, PREPROCESS = clip.load("ViT-B/32", device=DEVICE)
 DIR_NAME = os.path.dirname(__file__)
 ROOT = os.path.abspath(os.path.join(DIR_NAME, os.pardir))
-IMAGES_PATH = os.path.join(ROOT, "image")
+IMAGES_PATH = os.path.join(ROOT, "data")
 
 DICT_IMAGE_PATH = get_image_path()
 BIN_FILE=os.path.join(ROOT + "/config/faiss_normal_ViT.bin")
@@ -22,8 +29,9 @@ BIN_FILE=os.path.join(ROOT + "/config/faiss_normal_ViT.bin")
 FAISS_TEST= Myfaiss(BIN_FILE, DICT_IMAGE_PATH, DEVICE, MODEL, Translation())
 
 
-def get_response_image(image_path):
-    img = cv2.imread(image_path)
+def get_response_image(id):
+    image_name = DICT_IMAGE_PATH[id]
+    img = cv2.imread(image_name)
 
     ret, jpeg = cv2.imencode('.jpg', img)
     encoded_img = base64.b64encode(jpeg).decode('ascii')
@@ -35,8 +43,8 @@ def faiss_image(query):
     
     text = query
 
-    scores, _, infos_query, images = FAISS_TEST.text_search(text, k=200)
+    scores, idx, infos_query, images = FAISS_TEST.text_search(text, k=200)
 
-    for image in images:
-        encoded_images[image] = get_response_image(os.path.join(IMAGES_PATH, image))
+    for id in idx:
+        encoded_images[int(id)] = get_response_image(id)
     return encoded_images
